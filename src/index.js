ba54b7b31ab6817a23dc1e1c425a5ea54c85b216
nakapol <nakapolpormmakoon@gmail.com>
2018-02-20 17:50:20 +0700

    TodoList-app-Test
    Test redux TodoList

--- src/index.js
+++ src/index.js
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
-import { render } from 'react-dom'
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
-// import todoApp from './reducers'
+import todoApp from './store/reducers'
 import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+let store = createStore(todoApp)
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
 registerServiceWorker()
